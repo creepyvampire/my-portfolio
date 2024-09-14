536f9c58d9d3af000805c67cdecd29a923159c8e
creepyvampire <arjunshastry1@gmail.com>
2024-09-14 22:03:24 +0530

    minor changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import logo from "../assets/logo.png";
 
 import { FaLinkedin, FaGithub } from 'react-icons/fa';
 
@@ -6,7 +7,7 @@ const Navbar = () => {
     return (
         <nav class=' mb-20 flex items-center justify-between py-6'>
             <div class='flex flex-shrink-0 items-center'>
-                <img src='https://placehold.co/75x75' alt='logo' />
+                <img class='max-w-20' src={logo} alt='logo' />
             </div>
             <div class='m-8 flex items-center justify-center gap-4 text-3xl'>
                 <FaGithub />
