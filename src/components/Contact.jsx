f870e08d007730f0119e2e736436c9f322357af2
creepyvampire <arjunshastry1@gmail.com>
2026-02-08 23:24:40 +0530

    Added contact email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
             <div class='border-b border-neutral-900 pb-24'>
                 <h1 class='my-10 text-center text-4xl'>Get in Touch</h1>
                 <div class='text-center tracking-tighter'>
-                    {/* <a href='#' class='border-b pb-4'>1da21cs025.cs@drait.edu.in</a> */}
+                    <a href='#' class='border-b pb-4'>arjunshastry1@gmail.com</a>
                 </div>
             </div>
         </>
